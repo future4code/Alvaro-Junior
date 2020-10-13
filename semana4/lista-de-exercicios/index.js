77ea344eea2cdfc79307d4b2e5b03def58fcd7fd
Alvaro Barros Junior <alvarojoaquimjr>
2020-10-12 22:57:15 -0300

    Continuo realizando a lista de exercícios, esse décimo segundo commit é do exercício 3 dos Exercícios de Funções de Array

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -326,3 +326,31 @@ array2.forEach((numero, index) => {
         parOuImpar.push(numero + " é impar,") 
     }
 })
+
+// EXERCÍCIO 3
+
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+// a.
+
+const temPermissão = pessoas.filter((pessoa, index, pessoas) => {
+    if (pessoa.altura >= 1.5){
+        return true
+    }
+})
+
+// b.
+
+const semPermissão = pessoas.filter((pessoa, index, pessoas) => {
+    if (pessoa.altura < 1.5){
+        return true
+    }
+})
+
