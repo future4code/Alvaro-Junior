a32cd57bee491a8423babe2b1795f461f478a1c3
Alvaro Barros Junior <alvarojoaquimjr>
2020-10-12 23:07:11 -0300

    Continuo realizando a lista de exercícios, esse décimo segundo commit é do exercício 4 dos Exercícios de Funções de Array

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -354,3 +354,30 @@ const semPermissão = pessoas.filter((pessoa, index, pessoas) => {
     }
 })
 
+// EXERCÍCIO 4
+
+const consultas = [
+	{ nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
+	{ nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
+	{ nome: "Paula", genero: "feminino", cancelada: true, dataDaConsulta: "03/11/2019" },
+	{ nome: "Márcia", genero: "feminino", cancelada: false, dataDaConsulta: "04/11/2019" }
+]
+
+const emails = []
+let pronomeTratamento
+let lembrete
+
+consultas.forEach((paciente) => {
+    if (paciente.genero === "masculino"){
+        pronomeTratamento = "Sr."
+        lembrete = "lembrá-lo"
+    }else{
+        pronomeTratamento = "Sra."
+        lembrete = "lembrá-la"
+    }
+    emails.push(`Olá, ${pronomeTratamento} ${paciente.nome}. Estamos enviando esta mensagem para
+    ${lembrete} da sua consulta no dia ${paciente.dataDaConsulta}. Por favor, acuse
+    o recebimento deste e-mail.`)
+})
+
+console.log(emails)
